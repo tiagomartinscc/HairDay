e8c120af1b0f443c92a738fcb837c9269165f527
Tiago Martins <tiagomartinscc@hotmail.com>
2024-07-06 18:00:51 -0300

    copy assets to dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
+const CopyWebPackPlugin = require("copy-webpack-plugin")
 
 module.exports = {
   target: "web",
@@ -23,6 +24,14 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "index.html"),
       favicon: path.resolve(__dirname, "src", "assets", "scissors.svg")
+    }),
+    new CopyWebPackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src", "assets"),
+          to: path.resolve(__dirname, "dist", "src", "assets")
+        }
+      ]
     })
   ],
 
